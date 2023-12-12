a7d912745598d645c27214b6035744c32275d2a5
JozeLuin <jozeluin@gmail.com>
2023-12-12 21:37:53 +0100

    Asus.12 12 23

--- app/src/main/java/com/example/twitterchallenger/MainActivity.kt
+++ app/src/main/java/com/example/twitterchallenger/MainActivity.kt
@@ -17,31 +17,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TwitterChallengerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MyTwiter()
-                }
-            }
+
+            MyTwiter()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TwitterChallengerTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/twitterchallenger/ui/MyTwiter.kt
+++ app/src/main/java/com/example/twitterchallenger/ui/MyTwiter.kt
@@ -3,15 +3,23 @@ package com.example.twitterchallenger.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+
+
 import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -19,11 +27,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextAlign.Companion.End
+import androidx.compose.ui.text.style.TextAlign.Companion.Start
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.twitterchallenger.R
 
-
+@Preview
 @Composable
 fun MyTwiter() {
     Column(
@@ -60,7 +72,7 @@ fun MyTwiter() {
 @Composable
 fun BodyTwiter() {
 
-    Row(Modifier.fillMaxSize()) {
+    Row() {
 
 
         Column(
@@ -75,7 +87,7 @@ fun BodyTwiter() {
                 painter = painterResource(id = R.drawable.profile),
                 contentDescription = "Imagen de la izquierda",
                 modifier = Modifier
-                    .size(100.dp)
+                    .size(75.dp)
                     .padding(10.dp)
                     .clip(
                         CircleShape
@@ -99,35 +111,48 @@ fun BodyTwiter() {
 
 @Composable
 fun Mensajes() {
-    Column(
-        Modifier
-            .fillMaxSize()
-            .padding(end = 40.dp, start = 10.dp)
-    ) {
-        Row(Modifier.weight(0.5f)) {
 
-            Text(text = "Aris", color = Color(0xff4ea8e9), textAlign = TextAlign.Right)
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(end = 20.dp)) {
+        Row(modifier = Modifier.padding(top = 15.dp)) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(0.5f),
+                text = "JLuis",
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                text = "@JLuis 4h",
+                //textAlign = Start,
+                color = Color.Gray
+            )
 
+            Icon(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 70.dp)
+
+
+                    .weight(1f),
+                painter = painterResource(id = R.drawable.ic_dots),
+                tint = Color.White,
+                contentDescription = "null" // decorative element,
+
+            )
         }
-        Divider(
-            Modifier
-                .background(Color(0xFF3C3B3B))
-                .height(1.dp)
-
-        )
-        Row(Modifier.weight(2.5f)) {
-
-        }
-        Divider(
-            Modifier
-                .background(Color(0xFF3C3B3B))
-                .height(1.dp)
-
-        )
-        Row(Modifier.weight(2.5f)) {
-
-        }
+        Spacer(modifier = Modifier.size(2.dp))
+        Text(modifier = Modifier.fillMaxWidth(),
+            text = "Descripcion larga sobre dwd",
+            color = Color.White, )
 
 
     }
+
+
 }
